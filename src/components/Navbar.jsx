c0a51cfb5f501bd0d52e2c2ed53c534908abb1e6
SHAIK ABDULHAMEED <shaikabdulhameedd@gmail.com>
2024-06-08 20:46:52 +0530

    Navbar css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,20 @@
-import me from "../assets/AH.png";
+import logo from "../assets/AH.png";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
-import { FaTwitter } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
 import { FaInstagram } from "react-icons/fa";
+
 const Navbar = () => {
   return (
-    <nav className=" mb-20 flex items-center justify-between py-6">
+    <nav className="mb-20 flex items-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center">
-        <img src={me} className="mx-2 w-9" alt="logo"/>
+        <img className="mx-2 w-10" src={logo} alt="logo" />
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-        <FaLinkedin/>
-        <FaGithub/>
-        <FaTwitter/>
-        <FaInstagram/>
+        <FaLinkedin />
+        <FaGithub />
+        <FaInstagram />
+        <FaSquareXTwitter />
       </div>
     </nav>
   );
