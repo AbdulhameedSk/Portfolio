5dfed5bbf78e870c3183ba2937fd14d043683673
SHAIK ABDULHAMEED <shaikabdulhameedd@gmail.com>
2024-06-08 20:25:52 +0530

    Added Hero

---
+++ src/components/Hero.jsx
@@ -0,0 +1,32 @@
+import { HERO_CONTENT } from "../constants";
+import profilePic from "../assets/pfp-bgrm.png";
+
+const Hero = () => {
+  return (
+    <div className="border-b border-neutral-900 pb-4 lg:mb-36">
+      <div className="flex flex-wrap">
+        <div className="w-full lg:w-1/2">
+          <div className="flex flex-col items-center lg:items-start">
+            <h2 className="pb-10 text-6xl font-thin tracking-tight lg:mt-16 lg:text-6xl">
+              SHAIK ABDULHAMEED
+            </h2>
+            {/* <span className="bg-gradient-to-r from-red-900 via-amber-50 to-green-900 bg-clip-text text-3xl tracking-tight text-transparent"> */}
+            <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent">
+              Full Stack Developer
+            </span>
+            <p className="my-2 max-w-xl py-6 font-light tracking-tighter">
+              {HERO_CONTENT}
+            </p>
+          </div>
+        </div>
+        <div className="w-full lg:w-1/2 lg:p-8">
+          <div className="flex justify-center">
+            <img src={profilePic} alt="PFP" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
