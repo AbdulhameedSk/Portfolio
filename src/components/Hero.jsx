6fefabefa8fc5f295747587ff301be56e93d8722
SHAIK ABDULHAMEED <shaikabdulhameedd@gmail.com>
2024-06-08 20:40:30 +0530

    fixed css

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,10 +7,9 @@ const Hero = () => {
       <div className="flex flex-wrap">
         <div className="w-full lg:w-1/2">
           <div className="flex flex-col items-center lg:items-start">
-            <h2 className="pb-10 text-6xl font-thin tracking-tight lg:mt-16 lg:text-6xl">
-              SHAIK ABDULHAMEED
+            <h2 className="responsive-text pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-6xl">
+              ABDULHAMEED
             </h2>
-            {/* <span className="bg-gradient-to-r from-red-900 via-amber-50 to-green-900 bg-clip-text text-3xl tracking-tight text-transparent"> */}
             <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent">
               Full Stack Developer
             </span>
