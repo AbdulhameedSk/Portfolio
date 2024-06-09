e4a03f6b7976b0fdf8087bdd15e2678d32e88f32
SHAIK ABDULHAMEED <shaikabdulhameedd@gmail.com>
2024-06-10 01:36:38 +0530

    added links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,7 @@ import logo from "../assets/AH.png";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
-import { FaInstagram } from "react-icons/fa";
-
+import { SiLeetcode } from "react-icons/si";
 const Navbar = () => {
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
@@ -11,10 +10,33 @@ const Navbar = () => {
         <img className="mx-2 w-10" src={logo} alt="logo" />
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-        <FaLinkedin />
-        <FaGithub />
-        <FaInstagram />
-        <FaSquareXTwitter />
+        <FaLinkedin
+          onClick={() =>
+            window.open(
+              "https://www.linkedin.com/in/shaikabdulhameed",
+              "_blank"
+            )
+          }
+          style={{ cursor: "pointer" }}
+        />
+        <FaGithub
+          onClick={() =>
+            window.open("https://www.github.com/abdulhameedsk", "_blank")
+          }
+          style={{ cursor: "pointer" }}
+        />
+        <SiLeetcode
+          onClick={() =>
+            window.open("https://leetcode.com/SHAIKABDULHAMEED", "_blank")
+          }
+          style={{ cursor: "pointer" }}
+        />
+        <FaSquareXTwitter
+          onClick={() =>
+            window.open("https://twitter.com/CodeAce_Abdul", "_blank")
+          }
+          style={{ cursor: "pointer" }}
+        />
       </div>
     </nav>
   );
