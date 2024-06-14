905c598ff6c5246867d4ae055908b513196e6790
SHAIK ABDULHAMEED <shaikabdulhameedd@gmail.com>
2024-06-14 11:08:34 +0530

    added framer-motion for about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,6 @@
 import aboutImg from "../assets/aboutpfpr.png";
 import { ABOUT_TEXT } from "../constants";
+import {motion} from "framer-motion";
 const About = () => {
   return (
     <div className="border-b border-neutral-900 pb-4">
@@ -8,16 +9,16 @@ const About = () => {
         <span className="text-neutral-500"> ME</span>
       </h2>
       <div className="flex flex-wrap">
-        <div className="w-full lg:w-1/2 lg:p-8">
+        <motion.div whileInView={{opacity:1,x:0}} initial={{opacity:0,x:-100}} transition={{duration:0.5}} className="w-full lg:w-1/2 lg:p-8">
           <div className="flex items-center justify-center">
             <img className="rounded-2xl" src={aboutImg} alt="About-IMG" />
           </div>
-        </div>
-        <div className="w-full lg:w-1/2 py-28">
+        </motion.div>
+        <motion.div whileInView={{opacity:1,x:0}} initial={{opacity:0,x:100}} transition={{duration:1}} className="w-full lg:w-1/2 py-28">
           <div className="flex justify-center lg:justify-start">
             <p className="my-2 max-w-xl py-6">{ABOUT_TEXT}</p>
           </div>
-        </div>
+        </motion.div>
       </div>
     </div>
   );
